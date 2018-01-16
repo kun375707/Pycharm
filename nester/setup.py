84c2f28dbc28ffed72d0e492fd988f55bb029eca
kun375707 <1137111860@qq.com>
2018-01-16 16:52:38 +0800

    first commit

---
+++ nester/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(
+    name = 'nester',
+    version = '1.0.0',
+    py_modules = ['nester'],
+    autor = 'mm',
+    autor_email = 'xxx',
+    url = 'xxx',
+    descripption = 'A simple printer of nested lists')
