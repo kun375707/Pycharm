fa1b700ddec0e304af6b728414fcc75550fced96
kun375707 <1137111860@qq.com>
2018-02-07 17:08:23 +0800

    通过CGI程序传递 Textarea 数据

---
+++ M/Demo10.py
@@ -0,0 +1,26 @@
+'''通过CGI程序传递 Textarea 数据'''
+#Textarea 向服务器传递多行数据
+
+# 引入 CGI 处理模块
+import cgi, cgitb
+
+# 创建 FieldStorage的实例
+form = cgi.FieldStorage()
+
+# 接收字段数据
+if form.getvalue('textcontent'):
+   text_content = form.getvalue('textcontent')
+else:
+   text_content = "没有内容"
+
+print("Content-type:text/html")
+print()
+print("<html>")
+print("<head>")
+print("<meta charset=\"utf-8\">")
+print("<title> CGI 测试实例</title>")
+print("</head>")
+print("<body>")
+print("<h2> 输入的内容是 : %s</h2>" % text_content)
+print("</body>")
+print("</html>")
